4c21de8c3b7efe5eadd9e3976eb1da2d91ccac5a
Shen Chen <schen89@live.com>
2018-01-19 16:31:45 -0500

    Update load.php

--- Ysphp/load.php
+++ Ysphp/load.php
@@ -1,7 +1,10 @@
 <?php 
 
     spl_autoload_register(function($class_name){
-        require dirname(__FILE__) . ( str_replace(array('\\','Ysphp'), array('/Lib/',''), $class_name) . '.php' ) ;
+        $path = dirname(__FILE__) . ( str_replace(array('\\','Ysphp'), array('/Lib/',''), $class_name) . '.php' ) ;
+        if ( file_exists( $path ) ){
+            require $path;
+        }
     });
     
 ?>
